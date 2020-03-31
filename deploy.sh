73c39d6a8137683ba50f66022abc244faff79735
mauricio <mauricio@outlook.com>
2020-03-31 18:29:43 -0300

    fixed raised during after feedbacks

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,15 @@
 
 # build the application
 echo "Starting covid-favor build"
-ionic build --prod
+ionic build
 echo "covid-favor build completed"
 
 # deploy to S3
 echo "Starting covid-favor deployment to S3"
 cd ./www && aws s3 cp . s3://codecraft-covid-favor --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
 echo "covid-favor deployment to S3 completed"
+
+# creating invalidation
+echo "Creating invalidation for website"
+aws cloudfront create-invalidation --distribution-id E2URYH50YBZ9A1 --paths /*
+echo "Invalidation request is completed!"
