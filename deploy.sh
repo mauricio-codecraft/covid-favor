515ec898733f02bac86de8cc3df8675cd8d88eca
mauricio <mauricio@outlook.com>
2020-04-01 14:52:05 -0300

    adding minification

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,21 @@
 # -----------------------------------------------------------------------------
 
 # build the application
-echo "Starting covid-favor build"
+echo "Starting covid-favor build..."
 ionic build
 echo "covid-favor build completed"
 
+cd ./www
+
+# compressing files
+echo "Starting to minify files..."
+for f in *.js; do short=${f%.js}; terser $f > $short.min.js && rm $f && mv $short.min.js $f; done
+echo "Minification is completed!"
+
 # deploy to S3
 echo "Starting covid-favor deployment to S3"
-cd ./www && aws s3 cp . s3://codecraft-covid-favor --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
-echo "covid-favor deployment to S3 completed"
+aws s3 cp . s3://codecraft-covid-favor --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers --recursive
+echo "covid-favor deployment to S3 completed!"
 
 # creating invalidation
 echo "Creating invalidation for website"
